41efe853671ce4b31df82f30dbd06369630da46f
Ivan Zh <ivzhurau@gmail.com>
2020-04-17 12:30:07 +0300

    added class for a cup and base class for figures

---
+++ Tetris/Models/Cup.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    class Cup
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public int LeftMarginWidth { get; set; }
+        public bool[,] Items { get; set; }
+
+        public Cup() : this (0, 0) 
+        { }
+
+        public Cup(int w, int h, int leftMargin = 0)
+        {
+            Items = new bool[w, h];
+        }
+
+        private void DrawMargin()
+        {
+            Console.Write(new string(' ', LeftMarginWidth));
+        }
+
+        public void Draw()
+        {
+            Console.WriteLine('┌' + new string(' ', Width) + '┐');
+            for (int i = 0; i < Height; i++)
+            {
+                DrawMargin();
+                Console.WriteLine('|' + new string(' ', Width) + '|');
+            }
+            Console.WriteLine('└' + new string('_', Width) + '┘');
+        }
+
+    }
+}
---
+++ Tetris/Models/Figure.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public abstract class Figure
+    {
+        // center of rotation and position
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Figure() : this(0, 0) 
+        { }
+
+        public Figure(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public abstract void Rotate();
+        public abstract void Update();
+        public abstract void Draw();
+        public abstract void Stop();
+    }
+}
