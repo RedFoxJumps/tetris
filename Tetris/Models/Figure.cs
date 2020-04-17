b9e2de3af4b91452b65a065b2c7e59e4ef008344
Ivan Zh <ivzhurau@gmail.com>
2020-04-17 20:46:38 +0300

    added T figure

--- Tetris/Models/Figure.cs
+++ Tetris/Models/Figure.cs
@@ -8,21 +8,37 @@ namespace Models
 {
     public abstract class Figure
     {
-        // center of rotation and position
+        // center of rotation
+        public int CX { get; set; }
+        public int CY { get; set; }
+
+        // position
         public int X { get; set; }
         public int Y { get; set; }
 
-        public Figure() : this(0, 0) 
+        public int LeftMarginWidth { get; set; }
+        public ConsoleColor FigureColor { get; set; }
+        public int[,] Points;
+
+        public int this[int i, int j]
+        {
+            get => Points[i, j];
+        }
+
+        public Figure() : this(0, 0)
         { }
 
-        public Figure(int x, int y)
+        public Figure(int cx, int cy, int margin = 0, int x = 0, int y = 0)
         {
+            CX = cx;
+            CY = cy;
             X = x;
             Y = y;
+            LeftMarginWidth = margin;
         }
 
         public abstract void Rotate();
-        public abstract void Update();
+        public abstract void Update(Cup cup);
         public abstract void Draw();
         public abstract void Stop();
     }
---
+++ Tetris/Models/TFig.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class TFig : Figure
+    {
+        public TFig() : this (0)
+        { }
+
+        public TFig(int margin) : base(1, 1, margin)
+        {
+            FigureColor = ConsoleColor.Yellow;
+
+            Points = new int[4, 4]
+            {
+                { 0, 0, 0, 0 },
+                { 1, 1, 1, 0 },
+                { 0, 1, 0, 0 },
+                { 0, 0, 0, 0 }
+            };
+        }
+
+        public override void Draw()
+        {
+
+        }
+
+        public override void Rotate()
+        {
+        }
+
+        public override void Stop()
+        {
+
+        }
+
+        public override void Update(Cup cup)
+        {
+
+        }
+    }
+}
